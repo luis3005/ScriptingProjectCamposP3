b9f0ef9ff42b3a6c0c54d0eace77fbbe2482981b
luis3005 <727988@student.dusd.net>
2022-10-26 10:46:58 -0700

    Added projecrt
    Added project

---
+++ ScriptingProjectCampos/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperarture = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+            
+            coffeeTemperature -= Time.deltaTime * 5f;
+    }
+    
+     void TemperatureTest()
+    {
+
+       if (coffeeTemperature > hotLimitTemperarture)
+        {
+          print("Coffee is too hot.");
+        }
+
+        else if  (coffeeTemperature < coldLimitTemperature)
+        {
+          print("Coffee is too cold.");
+        }
+        
+        else
+        {
+          print("Coffee is just right.");
+        }
+    }
+}
---
+++ ScriptingProjectCampos/Assets/Scripts/VisualandScripts.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+using System.Collections;
+
+
+public class VisualandScripts : MonoBehaviour
+{
+    int myInt = 5;
+
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
+   
